e962109fc3ef7ec443d6f13b05131bf693bee0cf
Ammar Najjar <najjarammar@gmail.com>
2016-06-30 14:54:01 +0200

    clean neovim, add aliases to bashrc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 rm -rf $HOME/.vim $HOME/.vimrc
-rm -rf $HOME/.config/nvim
+rm $HOME/.Xresources
 mkdir -p $HOME/.config
 
 id="$(cat /etc/*release | grep ID=)"
@@ -11,97 +11,61 @@ then
 	sudo dnf update -y
 	sudo dnf -y install vim vim-X11 git libtool autoconf automake cmake gcc gcc-c++ \
 		make pkgconfig unzip python-pip redhat-rpm-config clang \
-		powerline kernel-devel python-devel the_silver_searcher
-	# sudo pip install neovim
+		powerline kernel-devel python-devel the_silver_searcher ctags
 	echo "source '/usr/share/tmux/powerline.conf'" > $HOME/.tmux.conf
 elif [[ "$id" == *"debian"* ]]
 then
-	# Debian based install
+	# Debian based install dependencies
 	sudo apt-get update && sudo apt-get install -y \
 	vim vim-gtk git silversearcher-ag exuberant-ctags clang	powerline \
 	build-essential libtool
-	# sudo pip install neovim
 	echo "source '/usr/share/powerline/bindings/tmux/powerline.conf'" > $HOME/.tmux.conf
 elif [[ "$id" == *"ubuntu"* ]]
 then
-	# Ubuntu based install
+	# Ubuntu based install dependencies
 	sudo apt-get update && sudo apt-get install -y \
 	vim vim-gtk git silversearcher-ag exuberant-ctags clang build-essential \
 	automake python-dev python-pip python3-dev python3-pip libtool
-	# sudo pip install neovim
 fi
 
 current_dir=$(pwd)
 vim_dir="$current_dir/dotfiles"
-bin_dir="$vim_dir/neovim_bin"
-source_dir="$vim_dir/neovim_source"
 if [ -d $vim_dir ]
 then
 	rm -rf $vim_dir
 fi
 mkdir -p $vim_dir
-
 cd $vim_dir
+
 git clone https://github.com/ammarnajjar/dotfiles.git .
 
-# # install neovim
-# git clone https://github.com/neovim/neovim.git $source_dir
-# cd $source_dir
-# make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$bin_dir"
-# make install
-# ln -s $vim_dir $HOME/.config/nvim
-# sudo ln -s "$bin_dir/bin/nvim" /usr/local/bin/nvim
-
-cd $vim_dir
-# # install powerline fonts
-# git clone https://github.com/powerline/fonts fonts
-# sh fonts/install.sh
-
-# install bash-sensible (myfork)
-# git clone https://github.com/mrzool/bash-sensible.git
+# install bash-sensible && liquid prompt (myforks)
 git clone -b 'ignored-in-history' https://github.com/ammarnajjar/bash-sensible.git
-if [ -f $HOME/.bashrc ]
-then
-	echo 'source ~/.vim/bash-sensible/sensible.bash' >> ~/.bashrc
-fi
-
-# install liquid prompt
-# git clone https://github.com/nojhan/liquidprompt.git liquidprompt
 git clone -b twolinedprompt https://github.com/ammarnajjar/liquidprompt.git liquidprompt
-if [ -f $HOME/.bashrc ]
-then
-	echo '[[ $- = *i* ]] && source ~/.vim/liquidprompt/liquidprompt' >> ~/.bashrc
-	echo '[[ $- = *i* ]] && source ~/.vim/liquidprompt/liquid.theme' >> ~/.bashrc
-	source ~/.bashrc
-fi
 
-if [ -f $HOME/.tmux.conf ]
-then
-	echo "set -g history-limit 1000000" >> $HOME/.tmux.conf
-else
-	echo "set -g history-limit 1000000" > $HOME/.tmux.conf
-fi
+echo 'function mkcd() { mkdir $1; cd $1; }'
+echo 'alias ctagsit="ctags --append=no --recurse --totals --exclude=blib --exclude=.svn --exclude=.get --exclude='@.gitignore' --extra=q"' >> ~/.bashrc
+echo 'alias g="git status"' >> ~/.bashrc
+echo 'source ~/.vim/bash-sensible/sensible.bash' >> ~/.bashrc
+echo '[[ $- = *i* ]] && source ~/.vim/liquidprompt/liquidprompt' >> ~/.bashrc
+echo '[[ $- = *i* ]] && source ~/.vim/liquidprompt/liquid.theme' >> ~/.bashrc
+source ~/.bashrc
+
+echo "set -g history-limit 1000000" >> $HOME/.tmux.conf
 echo "set -s escape-time 0" >> $HOME/.tmux.conf
-rm $HOME/.Xresources
 
 # create symlinks
 ln -s $vim_dir $HOME/.vim
-ln -s $vim_dir/init.vim $HOME/.vimrc
+ln -s $vim_dir/vimrc.vim $HOME/.vimrc
 ln -s $vim_dir/Xresources $HOME/.Xresources
 
-# git config
+# git global aliases
 git config --global alias.lol 'log --graph --decorate --pretty=oneline --abbrev-commit --all'
+git config --global alias.st 'status'
 
-# # Install vim-plug
-# mkdir -p $vim_dir/plugged $vim_dir/undo
-# curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
-# 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-git clone https://github.com/ammarnajjar/wombat256mod.git $HOME/.vim/bundle/wombat256mod
-# vim +PlugInstall +qall
-
-# Install vundle
+# Install vundle & my colorscheme
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/ammarnajjar/wombat256mod.git $HOME/.vim/bundle/wombat256mod
 vim +PluginInstall +qall
 
 echo "Installation Complete"
