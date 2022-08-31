9f891b2ebb41aae9b3804a91a6b984c7f195bb72
Ammar Najjar <najjarammar@gmail.com>
2022-08-31 07:31:33 +0200

    greset function

--- shell/common.sh
+++ shell/common.sh
@@ -90,6 +90,12 @@ function mkcd() {
     cd -- "$*"
 }
 
+# fetch and reset hard the current branch
+function greset()
+{
+    git fetch --prune && git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
+}
+
 # display all ip addresses for this host
 function ips ()
 {
